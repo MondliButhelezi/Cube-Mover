b79fab085455fe1dfd126ed80a9dc2c9f2a7bd6c
Mondli Buthelezi <67345545+MondliButhelezi@users.noreply.github.com>
2022-07-15 13:22:55 +0200

    not empty

---
+++ Cube Mover/Assets/Cube.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+   
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * 15f, 0f, 0f);
+        transform.Translate(0f, 0f, Input.GetAxis("Vertical") * Time.deltaTime * 15f);
+    }
+}
